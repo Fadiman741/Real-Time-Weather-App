a74278905949e8f1cbd9835d2cab8ba25026920f
Mafadi Lephaka <mafadilephaka97@gmail.com>
2023-01-24 16:41:57 +0200

    upadate

--- src/app/apixu.service.ts
+++ src/app/apixu.service.ts
@@ -9,7 +9,7 @@ export class ApixuService {
   constructor(private http: HttpClient) { }
   getWeather(location: string){
     return this.http.get(
-        'http://api.weatherstack.com/current?access_key=ab4f4438f9e08d9b9328d1a6799f9d2b&query='+ location
+        'http://api.weatherstack.com/current?access_key=ab4f4438f9e08d9b9328d1a6799f9d2b&query=cape%20town'
     );
   }
 }
