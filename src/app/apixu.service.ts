6e1554df202d4f03714301924c1529c9d75ec278
Mafadi Lephaka <mafadilephaka97@gmail.com>
2023-02-04 19:08:49 +0200

    http client

--- src/app/apixu.service.ts
+++ src/app/apixu.service.ts
@@ -1,14 +1,25 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApixuService {
-
+  
+  headers: HttpHeaders = new HttpHeaders;
+  
   constructor(private http: HttpClient) { }
   getWeather(): Observable<any> {
-    return this.http.get('http://api.weatherstack.com/current?access_key=ab4f4438f9e08d9b9328d1a6799f9d2b&query=cape%20town');
+    
+    const httpOptions = {
+      headers: this.headers
+    };
+
+    this.headers.append('Access-Control-Allow-Origin', '*');
+    this.headers.append('Secure', 'false');
+
+
+    return this.http.get('http://api.weatherstack.com/current?access_key=ab4f4438f9e08d9b9328d1a6799f9d2b&query=cape%20town',httpOptions);
   }
 }
